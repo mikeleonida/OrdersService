270551d7942e0d1f52ca0d28e3b4aae986a989cf
mikeleonida <leoleo@alum.mit.edu>
2022-03-10 10:12:06 -0500

    pom.xml axon 4.5.3 etc

--- src/main/java/com/appsdeveloperblog/estore/OrdersService/OrdersServiceApplication.java
+++ src/main/java/com/appsdeveloperblog/estore/OrdersService/OrdersServiceApplication.java
@@ -4,11 +4,17 @@ import org.axonframework.config.Configuration;
 import org.axonframework.config.ConfigurationScopeAwareProvider;
 import org.axonframework.deadline.DeadlineManager;
 import org.axonframework.deadline.SimpleDeadlineManager;
+import org.axonframework.serialization.Serializer;
+import org.axonframework.serialization.xml.XStreamSerializer;
 import org.axonframework.spring.messaging.unitofwork.SpringTransactionManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.security.NoTypePermission;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -28,4 +34,27 @@ public class OrdersServiceApplication {
                 .build();
 	}
 
+//	@Bean
+//    XStream xstream(){
+//        XStream xstream = new XStream();
+//        // clear out existing permissions and set own ones
+//        xstream.addPermission(NoTypePermission.NONE);
+//        // allow any type from the same package
+//        xstream.allowTypesByWildcard(new String[] {
+//                "com.appsdeveloperblog.estore.core.**",
+//                "com.appsdeveloperblog.estore.OrdersService.**",
+//                "com.eventdriven.estore.**",
+//                "org.axonframework.**",
+//                "java.**",
+//                "com.thoughtworks.xstream.**"
+//        });
+//
+//        return xstream;
+//    }
+
+//    @Bean
+//    @Primary
+//    public Serializer serializer(XStream xStream) {
+//        return XStreamSerializer.builder().xStream(xStream).build();
+//    }
 }
--- src/main/java/com/appsdeveloperblog/estore/OrdersService/saga/OrderSaga.java
+++ src/main/java/com/appsdeveloperblog/estore/OrdersService/saga/OrderSaga.java
@@ -50,207 +50,192 @@ import com.thoughtworks.xstream.security.PrimitiveTypePermission;
 
 @Saga
 public class OrderSaga {
-	
+
 	@Autowired
 	private transient CommandGateway commandGateway;
-	
+
 	@Autowired
 	private transient QueryGateway queryGateway;
-	
+
 	@Autowired
 	private transient DeadlineManager deadlineManager;
-	
+
 	@Autowired
 	private transient QueryUpdateEmitter queryUpdateEmitter;
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(OrderSaga.class);
-	
-	private final String PAYMENT_PROCESSING_TIMEOUT_DEADLINE="payment-processing-deadline";
-	
+
+	private final String PAYMENT_PROCESSING_TIMEOUT_DEADLINE = "payment-processing-deadline";
+
 	private String scheduleId;
-	
-	@Autowired
-	private transient XStream xstream = new XStream();
-	
+
+//	@Autowired
+//	private transient XStream xstream = new XStream();
+//	
 	// init (non-static) block
-	{
+	//{
 //		// clear out existing permissions and set own ones
 //		xstream.addPermission(NoTypePermission.NONE);
 //		// allow some basics
 //		xstream.addPermission(NullPermission.NULL);
 //		xstream.addPermission(PrimitiveTypePermission.PRIMITIVES);
 //		xstream.allowTypeHierarchy(Collection.class);
-		
-		XStream.setupDefaultSecurity(xstream);
+
+		// XStream.setupDefaultSecurity(xstream);
+
 		// allow any type from core package
-		xstream.allowTypesByWildcard(new String[] {
-		    "com.appsdeveloperblog.estore.core.**"
-		});
-	}
-	
+		// xstream.allowTypesByWildcard(new String[] { "com.appsdeveloperblog.estore.core.**" });
+	//}
+
 	@StartSaga
-	@SagaEventHandler(associationProperty="orderId")
+	@SagaEventHandler(associationProperty = "orderId")
 	public void handle(OrderCreatedEvent orderCreatedEvent) {
 
 		ReserveProductCommand reserveProductCommand = ReserveProductCommand.builder()
-				.orderId(orderCreatedEvent.getOrderId())
-				.productId(orderCreatedEvent.getProductId())
-				.quantity(orderCreatedEvent.getQuantity())
-				.userId(orderCreatedEvent.getUserId())
-				.build();
-		
-		LOGGER.info("OrderCreatedEvent handled for orderId: " + reserveProductCommand.getOrderId() + 
-				" and productId: " + reserveProductCommand.getProductId() );
-		
+				.orderId(orderCreatedEvent.getOrderId()).productId(orderCreatedEvent.getProductId())
+				.quantity(orderCreatedEvent.getQuantity()).userId(orderCreatedEvent.getUserId()).build();
+
+		LOGGER.info("OrderCreatedEvent handled for orderId: " + reserveProductCommand.getOrderId() + " and productId: "
+				+ reserveProductCommand.getProductId());
+
 		commandGateway.send(reserveProductCommand, new CommandCallback<ReserveProductCommand, Object>() {
 
 			@Override
 			public void onResult(CommandMessage<? extends ReserveProductCommand> commandMessage,
 					CommandResultMessage<? extends Object> commandResultMessage) {
-				   if(commandResultMessage.isExceptional()) {
-					   // Start a compensating transaction
-						RejectOrderCommand rejectOrderCommand = new RejectOrderCommand(orderCreatedEvent.getOrderId(),
-								commandResultMessage.exceptionResult().getMessage());
-						
-						commandGateway.send(rejectOrderCommand);
-				   }
-				
+				if (commandResultMessage.isExceptional()) {
+					// Start a compensating transaction
+					RejectOrderCommand rejectOrderCommand = new RejectOrderCommand(orderCreatedEvent.getOrderId(),
+							commandResultMessage.exceptionResult().getMessage());
+
+					commandGateway.send(rejectOrderCommand);
+				}
+
 			}
-			
+
 		});
-		
- 
+
 	}
-	
-	@SagaEventHandler(associationProperty="orderId")
+
+	@SagaEventHandler(associationProperty = "orderId")
 	public void handle(ProductReservedEvent productReservedEvent) {
 		// Process user payment
-        LOGGER.info("ProductReserveddEvent is called for productId: "+ productReservedEvent.getProductId() + 
-        		" and orderId: " + productReservedEvent.getOrderId());
-        
-        FetchUserPaymentDetailsQuery fetchUserPaymentDetailsQuery = 
-        		new FetchUserPaymentDetailsQuery(productReservedEvent.getUserId());
-        
-        User userPaymentDetails = null;
-        
-        try {
-        	userPaymentDetails = queryGateway.query(fetchUserPaymentDetailsQuery, ResponseTypes.instanceOf(User.class)).join();
-        } catch(Exception ex) {
-        	LOGGER.error(ex.getMessage());
-        	
-        	// Start compensating transaction
-        	cancelProductReservation(productReservedEvent,ex.getMessage());
-        	return;
-        }
-        
-        if(userPaymentDetails == null) {
-        	// Start compensating transaction
-        	cancelProductReservation(productReservedEvent,"Could not fetch user payment details");
-        	return;
-        }
-        
-        LOGGER.info("Successfully fetched user payment details for user " + userPaymentDetails.getFirstName());
-        
-        
-       scheduleId =  deadlineManager.schedule(Duration.of(120, ChronoUnit.SECONDS),
-        		PAYMENT_PROCESSING_TIMEOUT_DEADLINE, productReservedEvent);
-       
-        ProcessPaymentCommand proccessPaymentCommand = ProcessPaymentCommand.builder()
-        		.orderId(productReservedEvent.getOrderId())
-        		.paymentDetails(userPaymentDetails.getPaymentDetails())
-        		.paymentId(UUID.randomUUID().toString())
-        		.build();
-        
-        String result = null;
-        try {
-        result = commandGateway.sendAndWait(proccessPaymentCommand);
-        } catch(Exception ex) {
-        	LOGGER.error(ex.getMessage());
-        	// Start compensating transaction
-        	cancelProductReservation(productReservedEvent,ex.getMessage());
-        	return;
-        }
-        
-        if(result == null) {
-        	LOGGER.info("The ProcessPaymentCommand resulted in NULL. Initiating a compensating transaction");
-        	// Start compensating transaction
-        	cancelProductReservation(productReservedEvent, "Could not proccess user payment with provided payment details"); 
-        }
- 
+		LOGGER.info("ProductReserveddEvent is called for productId: " + productReservedEvent.getProductId()
+				+ " and orderId: " + productReservedEvent.getOrderId());
+
+		FetchUserPaymentDetailsQuery fetchUserPaymentDetailsQuery = new FetchUserPaymentDetailsQuery(
+				productReservedEvent.getUserId());
+
+		User userPaymentDetails = null;
+
+		try {
+			userPaymentDetails = queryGateway.query(fetchUserPaymentDetailsQuery, ResponseTypes.instanceOf(User.class))
+					.join();
+		} catch (Exception ex) {
+			LOGGER.error(ex.getMessage());
+
+			// Start compensating transaction
+			cancelProductReservation(productReservedEvent, ex.getMessage());
+			return;
+		}
+
+		if (userPaymentDetails == null) {
+			// Start compensating transaction
+			cancelProductReservation(productReservedEvent, "Could not fetch user payment details");
+			return;
+		}
+
+		LOGGER.info("Successfully fetched user payment details for user " + userPaymentDetails.getFirstName());
+
+		scheduleId = deadlineManager.schedule(Duration.of(120, ChronoUnit.SECONDS), PAYMENT_PROCESSING_TIMEOUT_DEADLINE,
+				productReservedEvent);
+
+		ProcessPaymentCommand proccessPaymentCommand = ProcessPaymentCommand.builder()
+				.orderId(productReservedEvent.getOrderId()).paymentDetails(userPaymentDetails.getPaymentDetails())
+				.paymentId(UUID.randomUUID().toString()).build();
+
+		String result = null;
+		try {
+			result = commandGateway.sendAndWait(proccessPaymentCommand);
+		} catch (Exception ex) {
+			LOGGER.error(ex.getMessage());
+			// Start compensating transaction
+			cancelProductReservation(productReservedEvent, ex.getMessage());
+			return;
+		}
+
+		if (result == null) {
+			LOGGER.info("The ProcessPaymentCommand resulted in NULL. Initiating a compensating transaction");
+			// Start compensating transaction
+			cancelProductReservation(productReservedEvent,
+					"Could not proccess user payment with provided payment details");
+		}
+
 	}
-	
+
 	private void cancelProductReservation(ProductReservedEvent productReservedEvent, String reason) {
-		
+
 		cancelDeadline();
-		
-		CancelProductReservationCommand publishProductReservationCommand = 
-				CancelProductReservationCommand.builder()
-				.orderId(productReservedEvent.getOrderId())
-				.productId(productReservedEvent.getProductId())
-				.quantity(productReservedEvent.getQuantity())
-				.userId(productReservedEvent.getUserId())
-				.reason(reason)
+
+		CancelProductReservationCommand publishProductReservationCommand = CancelProductReservationCommand.builder()
+				.orderId(productReservedEvent.getOrderId()).productId(productReservedEvent.getProductId())
+				.quantity(productReservedEvent.getQuantity()).userId(productReservedEvent.getUserId()).reason(reason)
 				.build();
-		
+
 		commandGateway.send(publishProductReservationCommand);
-		
+
 	}
-	
-	
-	@SagaEventHandler(associationProperty="orderId")
+
+	@SagaEventHandler(associationProperty = "orderId")
 	public void handle(PaymentProcessedEvent paymentProcessedEvent) {
-		
+
 		cancelDeadline();
-		
+
 		// Send an ApproveOrderCommand
-		ApproveOrderCommand approveOrderCommand = 
-				new ApproveOrderCommand(paymentProcessedEvent.getOrderId());
-		
+		ApproveOrderCommand approveOrderCommand = new ApproveOrderCommand(paymentProcessedEvent.getOrderId());
+
 		commandGateway.send(approveOrderCommand);
 	}
-	
+
 	private void cancelDeadline() {
-		if (scheduleId != null) { 
+		if (scheduleId != null) {
 			deadlineManager.cancelSchedule(PAYMENT_PROCESSING_TIMEOUT_DEADLINE, scheduleId);
 			scheduleId = null;
 		}
 	}
-	
+
 	@EndSaga
-	@SagaEventHandler(associationProperty="orderId")
+	@SagaEventHandler(associationProperty = "orderId")
 	public void handle(OrderApprovedEvent orderApprovedEvent) {
 		LOGGER.info("Order is approved. Order Saga is complete for orderId: " + orderApprovedEvent.getOrderId());
-	    //SagaLifecycle.end();
-		queryUpdateEmitter.emit(FindOrderQuery.class, query -> true, 
-				new OrderSummary(orderApprovedEvent.getOrderId(), 
-						orderApprovedEvent.getOrderStatus(),
-						""));
+		// SagaLifecycle.end();
+		queryUpdateEmitter.emit(FindOrderQuery.class, query -> true,
+				new OrderSummary(orderApprovedEvent.getOrderId(), orderApprovedEvent.getOrderStatus(), ""));
 	}
 
-	@SagaEventHandler(associationProperty="orderId")
+	@SagaEventHandler(associationProperty = "orderId")
 	public void handle(ProductReservationCancelledEvent productReservationCancelledEvent) {
 		// Create and send a RejectOrderCommand
 		RejectOrderCommand rejectOrderCommand = new RejectOrderCommand(productReservationCancelledEvent.getOrderId(),
 				productReservationCancelledEvent.getReason());
-		
+
 		commandGateway.send(rejectOrderCommand);
 	}
-	
+
 	@EndSaga
-	@SagaEventHandler(associationProperty="orderId")
+	@SagaEventHandler(associationProperty = "orderId")
 	public void handle(OrderRejectedEvent orderRejectedEvent) {
 		LOGGER.info("Successfully rejected order with id " + orderRejectedEvent.getOrderId());
-		
-		queryUpdateEmitter.emit(FindOrderQuery.class, query -> true, 
-				new OrderSummary(orderRejectedEvent.getOrderId(), 
-						orderRejectedEvent.getOrderStatus(),
-						orderRejectedEvent.getReason()));
+
+		queryUpdateEmitter.emit(FindOrderQuery.class, query -> true, new OrderSummary(orderRejectedEvent.getOrderId(),
+				orderRejectedEvent.getOrderStatus(), orderRejectedEvent.getReason()));
 	}
-	
-	@DeadlineHandler(deadlineName=PAYMENT_PROCESSING_TIMEOUT_DEADLINE)
+
+	@DeadlineHandler(deadlineName = PAYMENT_PROCESSING_TIMEOUT_DEADLINE)
 	public void handlePaymentDeadline(ProductReservedEvent productReservedEvent) {
-		LOGGER.info("Payment processing deadline took place. Sending a compensating command to cancel the product reservation");
+		LOGGER.info(
+				"Payment processing deadline took place. Sending a compensating command to cancel the product reservation");
 		cancelProductReservation(productReservedEvent, "Payment timeout");
 	}
-	
+
 }
